e1de88f11207642d72ca81276929c43cc13621f3
Darkcircle1105 <liuyqdarkcircle@gmail.com>
2025-05-16 05:18:30 +0100

    Update properties.js

--- src/properties.js
+++ src/properties.js
@@ -1,84 +1,65 @@
 /**
- * Name as used as title for the main page
+ * Title displayed at the top of the SurVis page
  * @type {string}
  */
-var title = 'SurVis Literature Collection';
+var title = 'Metaheuristic Approaches for Scheduling Sport Competitions';
 
 /**
- * Relative path to the data directory
+ * Relative path to the data directory (use './' if bib is in root)
  * @type {string}
  */
-var dataDir = 'data/';
+var dataDir = './';
 
 /**
- * Relative path to the js directory
+ * Relative path to the JavaScript files
  * @type {string}
  */
 var jsDir = 'js/';
 
 /**
- * Relative path to the styles directory
+ * Relative path to the stylesheets
  * @type {string}
  */
 var stylesDir = 'styles/';
 
 /**
- * Options for displaying tag clouds.
- * field: BibTeX field used for generating the tag clouds,
- * title: Title displayed as headline
- * minTagFrequency: minimum frequency of a tag to be displayed
- * @type {{field: string, title: string, minTagFrequency: number}[]}
+ * Options for displaying tag clouds
+ * Remove 'keywords' if not included in bib entries
  */
 var tagCloudOptions = [{
-    field: 'keywords',
-    title: 'Keywords',
-    minTagFrequency: 1
-}, {
     field: 'author',
     title: 'Authors',
     minTagFrequency: 1
 }, {
-    field: 'series',
-    title: 'Series',
+    field: 'year',
+    title: 'Year',
     minTagFrequency: 1
 }];
 
 /**
- * If BibTeX entries (and tags) should be editable
+ * Disable editing mode for final presentation
  * @type {boolean}
  */
-var editable = true;
+var editable = false;
 
 /**
- * Subtitle describing the paper the data is referring to (set to null to deactivate)
- * @type {{html: string, id: string}}
+ * Hide "paper" section below main title
+ * @type {null}
  */
-var paper = {
-    html: 'Sample literature collection for <b><a href="http://www.visus.uni-stuttgart.de/uploads/tx_vispublications/eurovis14-star.pdf" target="_blank">SurVis</a></b>',
-    id: 'Beck2016Visual'
-};
+var paper = null;
 
 /**
- * Extra pages like an about page that will be listed in the footer an can be opened on demand (page name -> path)
+ * No extra pages like about.html
  */
-var extraPages = {
-    'about': 'about.html'
-};
+var extraPages = {};
 
 /**
- * Custom style as path to an extra css file (leave empty if not applicable)
+ * No custom CSS
  * @type {string}
  */
 var customStyle = '';
-/* Example for simplified entries */
-//var customStyle = 'styles/custom_entries_simplified.css';
 
 /**
- * Options for showing citation information (set to null to avoid showing any citation information)
- * minCitationCount: minimum number of citations of a paper to be displayed in the visualization
- * @type {{minCitationCount: number}}
+ * Disable citation-based filtering
  */
-/* var citations = {
-    minCitationCount: 5
-}; */
 var citations = null;
